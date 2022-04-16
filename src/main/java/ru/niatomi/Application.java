a848e4dc8b8d9214ca55fb259b9a2c3b289e6e26
Niatomi <playervoker@gmail.com>
2022-04-16 22:24:38 +0400

    Add init of thread for algs in classes

--- src/main/java/ru/niatomi/Application.java
+++ src/main/java/ru/niatomi/Application.java
@@ -1,7 +1,26 @@
 package ru.niatomi;
 
+import ru.niatomi.classes.MyRunnableThread;
+import ru.niatomi.classes.MyThread;
+
 /**
  * @author niatomi
  */
 public class Application {
+    public static void main(String[] args) {
+
+        MyThread myThread1 = new MyThread();
+        Thread thread = new Thread(new MyRunnableThread());
+        myThread1.start();
+
+        try {
+//            thread.join();
+            myThread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("123");
+
+    }
 }
