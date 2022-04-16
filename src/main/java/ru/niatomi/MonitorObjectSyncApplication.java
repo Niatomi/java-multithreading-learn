0e2d1f8fa941a17ad45a5781d244cf848217e3ad
Niatomi <playervoker@gmail.com>
2022-04-16 22:50:54 +0400

    Synchronized monitor implemented

--- src/main/java/ru/niatomi/MonitorObjectSyncApplication.java
+++ src/main/java/ru/niatomi/MonitorObjectSyncApplication.java
@@ -4,4 +4,52 @@ package ru.niatomi;
  * @author niatomi
  */
 public class MonitorObjectSyncApplication {
+    public static void main(String[] args) {
+        Object object = new Object();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (object) {
+                    System.out.println("Thread 1 started");
+
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println("Thread 1 ended up");
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (object) {
+                    System.out.println("Thread 2 started");
+
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println("Thread 2 ended up");
+                }
+            }
+        });
+
+
+        try {
+            thread1.start();
+            thread2.start();
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
