c4d65815b5e644c56ec8f15d7de2114f1aa1b74c
Niatomi <playervoker@gmail.com>
2022-04-16 22:46:13 +0400

    Implemented incrementation and decrementation of counter

--- src/main/java/ru/niatomi/JointCounterApplication.java
+++ src/main/java/ru/niatomi/JointCounterApplication.java
@@ -1,7 +1,53 @@
 package ru.niatomi;
 
+import ru.niatomi.classes.Counter;
+
 /**
  * @author niatomi
  */
 public class JointCounterApplication {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+
+        Thread incrementThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++)
+                counter.increment();
+
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+        Thread decrementThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++)
+                counter.decrement();
+
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        try {
+            incrementThread.start();
+            decrementThread.start();
+            incrementThread.join();
+            decrementThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(counter.getX());
+    }
 }
